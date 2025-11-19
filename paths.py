6536c1300401e7c14449f64a2a739a8d618cd4e8
Kaush <superkaush@gmail.com>
2025-11-19 13:14:46 -0800

    paths edit

--- paths.py
+++ paths.py
@@ -7,3 +7,7 @@ STATIC_DIR = BASE_DIR / 'static'
 TRIPS_DIR = STATIC_DIR / 'Trips'
 CACHE_DIR = STATIC_DIR / 'cache'
 THUMBS_DIR = STATIC_DIR / 'thumbs'
+
+# Create them at startup
+for d in [TRIPS_DIR, CACHE_DIR, THUMBS_DIR]:
+    d.mkdir(parents=True, exist_ok=True)
